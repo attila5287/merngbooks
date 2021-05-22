26ddbbf9b326dc3e7d93bff5a05268f6f29ad714
attila5287 <atiturkoz@hotmail.com>
2021-05-22 09:14:23 -0600

    first success search only book.volumeInfo.title

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/books', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false
+});
 
 // Define API routes here
 
