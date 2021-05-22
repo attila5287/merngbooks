f7b0abdb90cd2db938f5aa5b76b24a35f6e7e933
attila5287 <atiturkoz@hotmail.com>
2021-05-22 10:04:26 -0600

    new build

---
+++ client/build/precache-manifest.72c538957b24a10776b7303ef61efd34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f8cd1b7d0f335e398b7cc0e1510039",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaee9e4d891d787ed03b",
+    "url": "/static/css/2.0326acd9.chunk.css"
+  },
+  {
+    "revision": "14817f30f38bfc43cd9c",
+    "url": "/static/css/main.347ec21f.chunk.css"
+  },
+  {
+    "revision": "eaee9e4d891d787ed03b",
+    "url": "/static/js/2.5ef57950.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5ef57950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14817f30f38bfc43cd9c",
+    "url": "/static/js/main.5fd8430b.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
